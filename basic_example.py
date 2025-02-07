b74533e631a33ead5d0a753b276cd4ddca971aef
samsniderheld <samsniderheld@gmail.com>
2025-02-07 09:35:25 -0500

    updating confing and prompting

--- basic_example.py
+++ basic_example.py
@@ -20,7 +20,8 @@ alex = get_agent_by_name("alex", synthetic_agents)
 bob = get_agent_by_name("bob", synthetic_agents)
 director = get_agent_by_name("director", synthetic_agents)
 
-prompt_agent = get_agent_by_name("prompt", helper_agents)
+img_prompt_agent = get_agent_by_name("img_prompt", helper_agents)
+vid_prompt_agent = get_agent_by_name("vid_prompt", helper_agents)
 
 character_agents = [alex, bob, director]
 
@@ -33,31 +34,32 @@ current_beat = 0
 
 interactive = False
 
-
 print("loading content generation capabilities")
 image_gen = FluxWrapper("black-forest-labs/FLUX.1-dev", "lora/cmbnd2.safetensors")
 video_gen = VideoWrapper(api="runway")
 print("loading complete")
 
-def generate_text_for_beat(beat_number):
+def generate_img_text_for_beat(beat_number):
     global all_scenes
     character_num = len(character_agents)
     concatenated_actions = " ".join(all_scenes[beat_number:beat_number+character_num])
-    prompt = prompt_agent.basic_api_call(concatenated_actions)
+    prompt = img_prompt_agent.basic_api_call(concatenated_actions)
     for agent in character_agents:
         if agent.name in prompt.lower():
             prompt += agent.flux_caption
     return prompt
 
-def get_default_text(beat_number):
-    return generate_text_for_beat(beat_number)
+def generate_vid_text_for_beat(img_text):
+    prompt = vid_prompt_agent.basic_api_call(img_text)
+    return prompt
 
 def update_textboxes():
     text_responses = []
     for i in range(story_beats):
-        default_text = get_default_text(i)
-        text_responses.append(default_text)
-        text_responses.append(default_text)  # Duplicate the text response
+        img_text = generate_img_text_for_beat(i)
+        text_responses.append(img_text)
+        vid_text = generate_vid_text_for_beat(img_text)
+        text_responses.append(vid_text)  
     return text_responses
 
 def generate_image(prompt):
@@ -68,69 +70,68 @@ def generate_video(prompt,image):
     path = video_gen.make_api_call(prompt,image)    
     return path
 
+def user(user_message, history: list):
+        history.append({"role": "user", "content": user_message})
+        return "", history
+
+def run_agents(history: list):
+    global current_beat
+    global story_beats
+    global all_scenes
+    global observation
+    global interactive
+    this_scene = []
+
+    message = history[-1]["content"]
+    if message == "interactive":
+        interactive = True
+
+    if interactive:
+        if current_beat < story_beats:
+            if message != "":
+                observation = message
+                all_scenes.append(observation)
+                this_scene.append(observation)
+
+            for agent in character_agents:
+                reflection = agent.reflect()
+                thought = f"<b style='color:green;'>{agent.name} thinks: {reflection}</b>"
+                history.append({"role": "assistant", "content": thought})
+                this_scene.append(reflection)
+                observation = agent.process_observation(observation, all_scenes, story_beats, current_beat)
+                all_scenes.append(observation)
+                this_scene.append(observation)
+
+            this_scene_actions = "\n ".join(this_scene)
+
+            current_beat += 1
+
+            history.append({"role": "assistant", "content": this_scene_actions + "\n\n would you like to interact?"})
+
+            yield history
+        else:
+            history.append({"role": "assistant", "content": "The scene is complete"})
+            yield history
+    else:
+        for i in range(story_beats):
+            for agent in character_agents:
+                time.sleep(3)
+                reflection = agent.reflect()
+                thought = f"<b style='color:green;'>{agent.name} thinks: {reflection}</b>"
+                history.append({"role": "assistant", "content": thought})
+                observation = agent.process_observation(observation, all_scenes, story_beats, i)
+                all_scenes.append(observation)
+                history.append({"role": "assistant", "content": observation})
+                yield history
+
+        history.append({"role": "assistant", "content": "The scene is complete"})
+        yield history
+
 
 with gr.Blocks() as demo:
     with gr.Tab("Simulation"):
         chatbot = gr.Chatbot(type="messages")
         msg = gr.Textbox()
-
-        def user(user_message, history: list):
-            history.append({"role": "user", "content": user_message})
-            return "", history
-
-        def run_agents(history: list):
-            global current_beat
-            global story_beats
-            global all_scenes
-            global observation
-            global interactive
-            this_scene = []
-
-            message = history[-1]["content"]
-            if message == "interactive":
-                interactive = True
-
-            if interactive:
-                if current_beat < story_beats:
-                    if message != "":
-                        observation = message
-                        all_scenes.append(observation)
-                        this_scene.append(observation)
-
-                    for agent in character_agents:
-                        reflection = agent.reflect()
-                        thought = f"<b style='color:green;'>{agent.name} thinks: {reflection}</b>"
-                        history.append({"role": "assistant", "content": thought})
-                        this_scene.append(reflection)
-                        observation = agent.process_observation(observation, all_scenes, story_beats, current_beat)
-                        all_scenes.append(observation)
-                        this_scene.append(observation)
-
-                    this_scene_actions = "\n ".join(this_scene)
-
-                    current_beat += 1
-
-                    history.append({"role": "assistant", "content": this_scene_actions + "\n\n would you like to interact?"})
-
-                    yield history
-                else:
-                    history.append({"role": "assistant", "content": "The scene is complete"})
-                    yield history
-            else:
-                for i in range(story_beats):
-                    for agent in character_agents:
-                        time.sleep(3)
-                        reflection = agent.reflect()
-                        thought = f"<b style='color:green;'>{agent.name} thinks: {reflection}</b>"
-                        history.append({"role": "assistant", "content": thought})
-                        observation = agent.process_observation(observation, all_scenes, story_beats, i)
-                        all_scenes.append(observation)
-                        history.append({"role": "assistant", "content": observation})
-                        yield history
-
-                history.append({"role": "assistant", "content": "The scene is complete"})
-                yield history
-
         msg.submit(user, [msg, chatbot], [msg, chatbot], queue=False).then(
             run_agents, chatbot, chatbot
         )
