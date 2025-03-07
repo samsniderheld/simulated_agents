77c6fd77161329bb1f6cc7bbc1d59cf59f07d91c
samsniderheld <samsniderheld@gmail.com>
2025-03-07 12:47:35 -0500

    Update flux_wrapper.py

--- content_generation/flux_wrapper.py
+++ content_generation/flux_wrapper.py
@@ -1,7 +1,9 @@
+import numpy as np
 import random
 import torch
 
-from diffusers import FluxPipeline, FluxTransformer2DModel
+from diffusers import FluxPipeline, FluxImg2ImgPipeline, FluxTransformer2DModel
+from PIL import Image
 from torchao.quantization import quantize_, int8_weight_only
 from sd_embed.embedding_funcs import get_weighted_text_embeddings_flux1
 
@@ -14,9 +16,10 @@ class FluxWrapper:
         lora_path (str): The path to the LoRA weights.
         pipe (FluxPipeline): The FluxPipeline object.
         transformer (FluxTransformer2DModel): The FluxTransformer2DModel object.
+        img2img (bool): Are we using the img2img process?
     """
 
-    def __init__(self, model_id: str, lora_paths: [str]) -> None:
+    def __init__(self, model_id: str, lora_paths: [str], img2img:bool=True) -> None:
         """
         Initializes the FluxWrapper with the specified model ID and LoRA weights path.
 
@@ -28,17 +31,24 @@ class FluxWrapper:
         self.lora_paths = lora_paths
         self.pipe = None
         self.transformer = None
+        self.img2img = img2img
         self.load_model()
 
     def load_model(self) -> None:
         """
         Loads the pre-trained model and LoRA weights, and quantizes the transformer model.
         """
-        self.pipe = FluxPipeline.from_pretrained(self.model_id, torch_dtype=torch.bfloat16)
-        for path in self.lora_paths:
-            self.pipe.load_lora_weights(path)
-        self.pipe.to("cuda")
 
+        if self.img2img:
+            self.pipe = FluxImg2ImgPipeline.from_pretrained(self.model_id, torch_dtype=torch.bfloat16)
+            for i,path in enumerate(self.lora_paths):
+                self.pipe.load_lora_weights(path,adapter_name=f"lora_{i}")
+            self.pipe.to("cuda")
+        else:
+            self.pipe = FluxPipeline.from_pretrained(self.model_id, torch_dtype=torch.bfloat16)
+            for i,path in enumerate(self.lora_paths):
+                self.pipe.load_lora_weights(path,adapter_name=f"lora_{i}")
+            self.pipe.to("cuda")
         self.transformer = FluxTransformer2DModel.from_pretrained(
             self.model_id,
             subfolder="transformer",
@@ -46,7 +56,7 @@ class FluxWrapper:
         )
         quantize_(self.transformer, int8_weight_only())
 
-    def generate_image(self, prompt: str, seed: int = None, width: int = 1024, height: int = 576, steps: int = 40) -> torch.Tensor:
+    def generate_image(self, prompt: str, seed: int = None, steps: int = 40, lora_0_weight: float = 0.3 ,lora_1_weight: float = 1.0, width: int = 1024, height: int = 576,img2img_str=0.7) -> torch.Tensor:
         """
         Generates an image based on the provided prompt.
 
@@ -66,12 +76,45 @@ class FluxWrapper:
         )
         if seed is None:
             seed = random.randint(0, 100000)
-        image = self.pipe(
-            prompt_embeds=prompt_embeds,
-            pooled_prompt_embeds=pooled_prompt_embeds,
-            num_inference_steps=steps,
-            generator=torch.Generator("cuda").manual_seed(seed),
-            width=width,
-            height=height
-        ).images[0]
+
+        self.pipe.set_adapters(["lora_0", "lora_1"], adapter_weights=[lora_0_weight, lora_1_weight])
+        
+        if self.img2img:
+
+            np_img = np.random.rand(width,height,3) * 255
+            noise_image = Image.fromarray(np_img.astype('uint8'))
+
+            # first generation, no img2img
+            image = self.pipe(
+                prompt_embeds=prompt_embeds,
+                pooled_prompt_embeds=pooled_prompt_embeds,
+                num_inference_steps=steps,
+                generator=torch.Generator("cuda").manual_seed(seed),
+                width=width,
+                height=height,
+                strength=1.0,
+                image=noise_image
+            ).images[0]
+
+            image = self.pipe(
+                prompt_embeds=prompt_embeds,
+                pooled_prompt_embeds=pooled_prompt_embeds,
+                num_inference_steps=steps,
+                generator=torch.Generator("cuda").manual_seed(seed),
+                width=width,
+                height=height,
+                strength=img2img_str,
+                image=image
+            ).images[0]
+            
+        else:
+            image = self.pipe(
+                prompt_embeds=prompt_embeds,
+                pooled_prompt_embeds=pooled_prompt_embeds,
+                num_inference_steps=steps,
+                generator=torch.Generator("cuda").manual_seed(seed),
+                width=width,
+                height=height
+            ).images[0]
+
         return image
